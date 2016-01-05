a2a1600b4741c6d292a3fdec22bd00e4dbb78ce5
Costas Smaragdakis <kesmarag@tem.uoc.gr>
2016-01-05 21:17:16 +0200

    update

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 megaptera v0.1b
 ===============
 
+Hidden Markov Model Clustering using Gaussian Mixture State Observation Densities
+---------------------------------------------------------------------------------
+
 GNU LESSER GENERAL PUBLIC LICENSE (LGPL)
 Version 3, 29 June 2007
 
--- src/main/org/kesmarag/megaptera/scripts/Main.kt
+++ src/main/org/kesmarag/megaptera/scripts/Main.kt
@@ -1,17 +1,21 @@
 package org.kesmarag.megaptera.scripts
 
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
 import org.kesmarag.megaptera.hmm.GhmmClustering
 import org.kesmarag.megaptera.utils.DataSet
+import java.io.File
 import java.io.FileOutputStream
+import java.io.FileWriter
 import java.io.ObjectOutputStream
 
 fun main(args: Array<String>) {
     println(".:: megaptera v0.1a ::.")
     //val pat: String? = readLine()
-    val dataSet = DataSet("/home/kesmarag/dev/test/dataSet/*.csv", 1)
+    val dataSet = DataSet("/home/kesmarag/dev/test/dataSet3/*.csv", 1)
     //val hmm = GaussianHiddenMarkovModel(dataSet, 4, 8, 0)
     //hmm.update()
-    val clustering = GhmmClustering(dataSet, 4, 2, 1)
+    val clustering = GhmmClustering(dataSet, 6, 2, 1)
     clustering.training()
     println("### Results ###")
     val fileOut = FileOutputStream("/home/kesmarag/clusters.ser")
@@ -19,13 +23,36 @@ fun main(args: Array<String>) {
     out.writeObject(clustering)
     out.close()
     fileOut.close()
+    /*
     dataSet.members
-            .filter { it.label == "data_101.csv" || it.label == "data_202.csv" }
+            .filter { it.label == "data_841.csv" || it.label == "data_1682.csv" }
             .sortedBy { it.label }
             .forEachIndexed { i, oS ->
                 println("${oS.label} => ${oS.ownerID} => ${oS.scores[0]} |" +
                         " ${oS.scores[1]} | ${oS.scores[2]}")
             }
+    */
+
+    var i_noisy: Int = 1682
+    dataSet.members.forEachIndexed { i, oSet -> if (oSet.label == "data_01682.csv") i_noisy = i }
+    println("i_noisy = ${i_noisy}")
+    var min = 1000.0
+    var argName: String = "none"
+    for (i in 0..dataSet.size-2){
+        val d = dist(dataSet.members[i].scores,dataSet.members[i_noisy].scores)
+        if (d < min && dataSet.members[i].label != "data_01682.csv"){
+            min = d
+            argName = dataSet.members[i].label
+        }
+    }
+    dataSet.members
+            //.filter { it.label == "data_841.csv" || it.label == "data_1682.csv" || it.label == argName }
+            .sortedBy { it.label }
+            .forEachIndexed { i, oS ->
+                println("${oS.label} => ${oS.ownerID} => ${oS.scores[0]} |" +
+                        " ${oS.scores[1]} | ${oS.scores[2]}")
+            }
+    println("argmin = $argName")
     //dataSet.members
     //.filter { it.ownerID == 1 }
     //        .sortedBy { it.label }
@@ -41,4 +68,33 @@ fun main(args: Array<String>) {
     // dataSet[0].scores.forEach { println(it) }
     // println("=== 1 ===")
     // dataSet[1].scores.forEach { println(it) }
+    val file = File("/home/kesmarag/results.csv")
+    val writer = FileWriter(file)
+    var records = CSVFormat.EXCEL
+    val csvFilePrinter = CSVPrinter(writer,records)
+    //csvFilePrinter.printRecord(listOf(1,2,5))
+    //csvFilePrinter.printRecord(listOf(10,21,52))
+    dataSet.members
+            //.filter { it.label == "data_841.csv" || it.label == "data_1682.csv" || it.label == argName }
+            .sortedBy { it.label }
+            .forEachIndexed { i, oS ->
+                println(oS.label)
+                csvFilePrinter.printRecord(oS.scores.toArrayList())
+            }
+
+    writer.flush();
+    writer.close();
+    csvFilePrinter.close();
+
+
+
+
+}
+
+fun dist(a1: DoubleArray,a2: DoubleArray): Double{
+    var s: Double = 0.0
+    for (n in 0..a1.size-1){
+        s += (a1[n]-a2[n])*(a1[n]-a2[n])
+    }
+    return s
 }
